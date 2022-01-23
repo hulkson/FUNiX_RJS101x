b7a9f07aa9a59477e17c5409fc1286b5859deeda
hulkson <quy.qtp@gmail.com>
2022-01-23 12:15:25 +0900

    update confusion app

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
+
 class Menu extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      selectedDish: null
-    }
-  }
-
-  onDishSelect(dish) {
-    this.setState({selectedDish: dish});
   }
 
   renderDish(dish) {
@@ -33,7 +26,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => {this.onDishSelect(dish)}}>
+          <Card>
               <CardImg width="100%" object src={dish.image} alt={dish.name} />
               <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
