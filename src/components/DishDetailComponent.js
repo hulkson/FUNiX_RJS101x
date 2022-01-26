8180f4f76f10282c131a2a9527794c0152daa3a3
hulkson <quy.qtp@gmail.com>
2022-01-27 02:29:08 +0900

    change to function component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,31 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardBody, CardText, CardTitle } from "reactstrap";
 import "./DishDetailComponent.css";
 
-class DishDetail extends Component {
-  render() {
-    if (this.props.dish === undefined) {
+
+  function RenderDish({dish}) {
+    if (dish === undefined) {
       return <div></div>;
     } else {
-      const selectedDish = this.props.dish;
       return (
         <div className="container selected-box">
           <Card className="col-lg-6 col-md-6 col-sm-12">
             <CardImg
               width="100%"
               object
-              src={selectedDish.image}
-              alt={selectedDish.name}
+              src={dish.image}
+              alt={dish.name}
             />
             <CardBody>
-              <CardTitle>{selectedDish.name}</CardTitle>
-              <CardText>{selectedDish.description}</CardText>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
             </CardBody>
           </Card>
             <Card className="col-lg-6 col-md-6 col-sm-12">
               <CardTitle>Comment</CardTitle>
               <CardText>
-                {this.props.dish.comments.map((comment) => {
+                {dish.comments.map((comment) => {
                   return (
                     <p key={comment.id}>
                       {comment.comment}
@@ -40,6 +39,5 @@ class DishDetail extends Component {
       );
     }
   }
-}
 
-export default DishDetail;
+export default RenderDish;
